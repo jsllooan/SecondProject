56a19729556ac2343c6657c51ae4ba71e70f5156
jsllooan <jsllooan@naver.com>
2017-07-05 10:28:30 +0900

    change contents Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 public static void main(String[] args){
-	System.out.println("Function1: what is git?");
-	System.out.println("Function2: ");
+	System.out.println("Function1: hello git : )");
+	System.out.println("Function2: what is git?");
 }
 }
