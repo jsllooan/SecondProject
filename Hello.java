d14eb110f30a139e9eacbed572e1a8d97d3e5b80
jsllooan <jsllooan@naver.com>
2017-07-05 12:43:04 +0900

    3rd change contents Hello program

--- Hello.java
+++ Hello.java
@@ -3,5 +3,7 @@ public static void main(String[] args){
 	System.out.println("Function1: hello git : )");
 	System.out.println("Function2: what is git?");
 	System.out.println("Function3: That's DVCS");
+	System.out.println("Function4: gitgitgitgit baby~ baby~ git ");
+
 }
 }
